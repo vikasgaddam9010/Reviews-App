10319b1b1daee6ede0f70ef0a22a2597159b3195
vikas <vikasgaddam9010@gmail.com>
2024-03-19 09:12:14 +0000

    Code giving error

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,68 @@
+import {Component} from 'react'
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {
+    id: 0,
+  }
+
+  onIncreament = () => {
+    const {id} = this.state
+    if (id > 0) {
+      this.setState(prevState => ({id: prevState.id - 1}))
+    }
+  }
+
+  onDecreament = () => {
+    const {id} = this.state
+    const {reviewsList} = this.props
+    if (id < reviewsList.length - 1) {
+      this.setState(prevState => ({id: prevState.id + 1}))
+    }
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    const {id} = this.state
+    const {imgUrl, username, companyName, description} = reviewsList[id]
+    return (
+      <div className="bg-container">
+        <div className="container">
+          <h1 className="username">Reviews</h1>
+          <div className="card">
+            <button
+              type="button"
+              onClick={this.onIncreament}
+              testid="leftArrow"
+              className="btn"
+            >
+              <img
+                alt="left arrow"
+                src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+              />
+            </button>
+            <div className="review-section">
+              <img alt={username} src={imgUrl} />
+              <p className="username">{username}</p>
+              <p>{companyName}</p>
+              <p>{description}</p>
+            </div>
+            <button
+              type="button"
+              onClick={this.onDecreament}
+              testid="rightArrow"
+              className="btn"
+            >
+              <img
+                alt="right arrow"
+                src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
